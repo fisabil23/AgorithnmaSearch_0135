8b060d983e2de7c55f11535310f3caa17d18c5ec
fisabil23 <161275234+fisabil23@users.noreply.github.com>
2024-04-05 10:13:55 +0700

    void input() {  while (true)  {   cout << "enter the number of element in the array:";   cin >> n;   if ((n > 0) && (n <= 20))    break;

--- pertemuan7.c++/pertemuan7.c++.cpp
+++ pertemuan7.c++/pertemuan7.c++.cpp
@@ -3,4 +3,15 @@ using namespace std;
 
 int arr[20]; //array to searched
 int n;       //number of elements in the array
-int i;       //index of array element   
+int i;       //index of array element
+
+void input()
+{
+	while (true)
+	{
+		cout << "enter the number of element in the array:";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+	}
+}
